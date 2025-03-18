4a2a8fea9cd4c6e09f947093f57fa3bc5288a10f
Emerson Bustamante <bustamante_emerson@hotmail.com>
2025-03-18 20:17:12 -0300

    Se implementa funcionalidad agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+const amigos = []; // Array para almacenar los nombres
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim(); // Eliminar espacios en blanco
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre); // Agregar al array
+    input.value = ""; // Limpiar el campo de entrada
+    actualizarLista();
+}
+
